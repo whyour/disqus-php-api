e355ccf8da96bea59f0c1c48710a8103681459b8
whyour <wangpengwnba@gmail.com>
2018-08-22 11:57:02 +0800

    update popular

--- api/popular.php
+++ api/popular.php
@@ -20,6 +20,7 @@ $curl_url = '/api/3.0/threads/listPopular.json?';
 $data = curl_get($curl_url, $fields);
 
 $posts = array();
+$thread = array();
 foreach ( $data -> response as $key => $post ) {
     $posts[$key] = array( 
         'id'=> $post -> id,
@@ -28,18 +29,17 @@ foreach ( $data -> response as $key => $post ) {
         'postsInInterval'=> $post -> postsInInterval,
         'posts'=> $post -> posts,
     );
-    $obj -> thread = $post -> id;
-    array_push($fields2, $obj);
+    $thread[] = $post -> id;
 }
+$fields2 -> thread = $thread;
 
 $curl_url2 = '/api/3.0/discovery/listTopPost.json?';
 $data2 = curl_get($curl_url2, $fields2);
 foreach ( $data2 -> response as $k => $p ) {
     foreach ( $posts as $i => $post) {
         if ($p -> id == $post -> thread) {
-            $obj1 -> avatar = $p -> avatar;
-            $obj2 -> message = $p -> message;
-            array_push($post, $obj1, $obj2);
+            $post -> avatar = $p -> avatar;
+            $post -> message = $p -> message;
         }
     }
 }
